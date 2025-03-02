e8a2a4150c072804ccc5a4f995613d1e151e3f86
jhakumar72 <mjha13503@gmail.com>
2025-03-02 02:28:35 +0000

    variable.js

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 12344
+let accountEmail = "mkjha@123.com"
+var accountPassword ="123423"
+accountCity ="madhubani"
+/*prefer not to use var 
+because of issue in block scope and functional scope
+*/
+let accountstate;
+//accountId = 2 // not allowed
+accountEmail = "manish@.com"
+accountPassword="3234232"
+accountCity;
+let accountState;
+acountName =("state bank")
+console.log(accountId);
+
+console.log(accountEmail);
+console.table([accountEmail,accountState,accountId,accountPassword,accountCity])
