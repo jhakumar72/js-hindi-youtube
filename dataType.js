71be32913beeea0d5d2fa95b5803ba9767fb76dd
jhakumar72 <mjha13503@gmail.com>
2025-03-02 02:57:28 +0000

    dataType.js

---
+++ dataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3+3)//we are using nodejs,not browser
+
+console.log(3+3)
+
+console.log("manish")
+
+let name ="manish"
+let age = 20
+let isLoggedIn = false
+let state;
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standlone value
+//undefined=>
+//symbol=> unique
+
+
+
+//object
+
+console.log(typeof "manish");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
