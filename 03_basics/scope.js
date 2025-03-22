5948f75da9846c1c7811c461361c2fab698c4b38
jhakumar72 <mjha13503@gmail.com>
2025-03-22 19:24:55 +0000

    scope.js

---
+++ 03_basics/scope.js
@@ -0,0 +1,51 @@
+// var c = 300
+let a =  300
+if (true){
+    let a = 10
+    // console.log("INNER:",a);
+    function addnum(){ const b = 20
+   
+
+    }
+}
+
+// console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+function one(){
+    const username = "manish"
+   function two(){
+    const website = "youtube"
+    // console.log(username);
+   
+   }
+    // console.log(website);
+    two()
+
+
+}
+ one()
+
+ if (true)  {
+    const username = "manish"
+    if(username ==="manish"){
+        const website = "youtube"
+        //console.log(username+website)
+    }
+ }
+
+
+ //++++++++++++++++++++++++++INTRESTING+++++++++++++++++++++++++
+console.log(addone(5))
+ function addone(num) {
+    return num +1
+ }
+
+  
+addTwoNumber(5)
+const addTwo = function(num){
+    return num +2
+}
+
