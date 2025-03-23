19f06a5d663ab70f4815b5d8ea20f53fd8349e17
jhakumar72 <mjha13503@gmail.com>
2025-03-23 03:35:23 +0000

    Arrow.js

---
+++ 03_basics/Arrow.js
@@ -0,0 +1,39 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username},welcome to website`);
+        console.log(this)
+
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+// console.log(this)
+
+// function chai(){
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () => {
+    let username = "hitesh"
+    console.log(this.username);
+}
+chai()
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2
+//  }
+// console.log(addTwo(7,5))
+
+// const addTwo = (num1,num2) => num1+num2
+// const addTwo = (num1,num2) => (num1+num2)
+const addTwo = (num1,num2) => ({username:"hitesh"})
+console.log(addTwo(4,3))
+
+
+
