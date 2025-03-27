970dc1af7c2196a590acf9953b451cd7df84933f
jhakumar72 <mjha13503@gmail.com>
2025-03-27 19:30:27 +0000

    one.js

---
+++ control_flow/one.js
@@ -0,0 +1,71 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+// if (temperature ===42){
+//     console.log("Less than 50");
+// }
+// else {
+// console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+// <,>, <=, >=, ==,!=,===,!==
+
+// const score = 200
+
+// if (score >100){
+//     const power = "fly"
+//     console.log(`User power:${power}`);
+// }
+// console.log(Ùser power:${power0}`);
+// const balance = 1000
+
+// if (balance>400) console.log("test"),console.log("test2");
+
+// if (balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 700){
+//     console.log("less than 700");
+// }
+// else if (balance < 800){
+//     console.log("less than 800");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+       
+
+// let userLoggedIn = true
+// let debitsCard = true
+// const loggedInFromGoogle = true
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitsCard){
+//     console.log("Allow to buy course");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("user logged in")
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    default: console.log("executed");
+        break;
+}
