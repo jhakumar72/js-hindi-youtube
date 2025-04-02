414c55e431ac3609f6a1dc567ee2634efcd0a9f1
jhakumar72 <mjha13503@gmail.com>
2025-04-02 12:43:14 +0000

    four.js

---
+++ iterations/four.js
@@ -0,0 +1,15 @@
+const myObject={
+    js:"Javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+
+for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programing = ["js","py","rb","java","cpp"]
+for (const key in programing) {
+   console.log(programing[key])
+}
